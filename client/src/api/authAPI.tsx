5048de518cbc4b1b8ee25ca4c394458573ec6bc6
Axe-P <axepax72@gmail.com>
2024-09-16 15:32:46 -0600

    Finished base project.

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,9 +1,32 @@
 import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
-  // TODO: make a POST request to the login route
-}
+  try {
+    const response = await fetch('/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userInfo),
+    });
 
+    if (!response.ok) {
+      const error = await response.json();
+      throw new Error(error.message || 'Login failed');
+    }
 
+    const data = await response.json();
+    if (data.token) {
+      localStorage.setItem('token', data.token);
+      console.log('Login successful, token stored:', data.token);
+      window.location.assign('/');
+    } else {
+      throw new Error('No token received from server');
+    }
+  } catch (error) {
+    console.error('Error during login:', error);
+    throw error;
+  }
+};
 
 export { login };
